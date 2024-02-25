66c3f6be3732c5c99681ea52e885361f4970d07c
Alexey Svistunov <xdayx53@gmail.com>
2024-02-25 11:31:54 +0300

    add sorting for all sales

--- src/components/Sale/AllSales.jsx
+++ src/components/Sale/AllSales.jsx
@@ -1,22 +1,81 @@
-
-import './Sale.css'
+import "./Sale.css";
 import { useGetProductsQuery } from "../../store/apiSlice";
 import ProductCard from "../ProductCard/ProductCard";
-import Sort from '../Sort/Sort'
+import Sort from "../Sort/Sort";
+import { useState } from "react";
 
 const AllSales = () => {
-  const query = useGetProductsQuery()
-  const data = query && query.data
-  const sales = data && data.filter(product => product['discont_price'])
+  const [priceFrom, setPriceFrom] = useState("");
+  const [priceTo, setPriceTo] = useState("");
+  const [sortBy, setSortBy] = useState("by default");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const query = useGetProductsQuery();
+  const data = query && query.data;
+  const sales = data && data.filter((product) => product["discont_price"]);
+
+  const applyFilters = ({ priceFrom, priceTo, showDiscounted, sortBy }) => {
+    let updatedProducts = [...sales];
+
+    if (priceFrom && priceTo) {
+      updatedProducts = updatedProducts.filter(
+        (product) => product.price >= priceFrom && product.price <= priceTo
+      );
+    }
+
+    if (showDiscounted) {
+      updatedProducts = updatedProducts.filter(
+        (product) => product["discont_price"]
+      );
+    }
+
+    switch (sortBy) {
+      case "newest":
+        updatedProducts.sort((a, b) => b.date - a.date);
+        break;
+      case "price_high_low":
+        updatedProducts.sort((a, b) => b.price - a.price);
+        break;
+      case "price_low_high":
+        updatedProducts.sort((a, b) => a.price - b.price);
+        break;
+      default:
+        break;
+    }
+
+    setFilteredProducts(updatedProducts);
+  };
   return (
     <section className="sale">
       <div className="container">
-        <h1 className="sale__title section-title sale__title-discounted">Discounted items</h1>
-        <Sort sale={true}/>
+        <h1 className="sale__title section-title sale__title-discounted">
+          Discounted items
+        </h1>
+        <Sort sale={true}
+          applyFilters={applyFilters}
+          priceFrom={priceFrom}
+          setPriceFrom={setPriceFrom}
+          priceTo={priceTo}
+          setPriceTo={setPriceTo}
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+         />
         <ul className="sale__list list-reset">
-        {sales && sales.map((product) => (
-          <ProductCard key={product.id} product={product} imgSrc={`../backend/public${product.image}`}></ProductCard>
-        ))}
+          {filteredProducts.length > 0
+            ? filteredProducts.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  imgSrc={`../backend/public${product.image}`}
+                />
+              ))
+            : sales && sales.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  imgSrc={`../backend/public${product.image}`}
+                />
+              ))}
         </ul>
       </div>
     </section>
